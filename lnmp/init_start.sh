5f3f52b69773d401a4f3d1a7f21a52b250b796fa
Stevie <stevie@ganguo.hk>
2015-12-08 20:35:47 +0800

    n

--- lnmp/init_start.sh
+++ lnmp/init_start.sh
@@ -2,6 +2,6 @@
 
 export PATH="$PATH:/opt/lnmp/varnish/bin:/opt/lnmp/sqlite/bin:/opt/lnmp/nginx/sbin:/opt/lnmp/php/bin:/opt/lnmp/mysql/bin:/opt/lnmp/common/bin"
 
-/opt/lnmp/ctlscript.sh start
+#/opt/lnmp/ctlscript.sh start
 
 exec
