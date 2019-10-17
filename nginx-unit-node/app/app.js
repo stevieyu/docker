fb21a3a72704d28b6e2f8bcd49a01029f53031e4
Stevie <yzm629@sina.cn>
2019-10-17 11:31:34 +0800

    update

--- nginx-unit-node/app/app.js
+++ nginx-unit-node/app/app.js
@@ -1,19 +1,36 @@
 #!/usr/bin/env node
 
-const {
-  createServer,
-  IncomingMessage,
-  ServerResponse,
-} = require('unit-http')
+const { createServer, IncomingMessage, ServerResponse } = require('unit-http')
 
 require('http').ServerResponse = ServerResponse
 require('http').IncomingMessage = IncomingMessage
 
+const express = () => {
+	const app = require('express')()
 
-const app = require('express')({
-	
-})
+	app.get('/', (req, res) => res.send('Hello, Unit! express'))
 
-app.get('/', (req, res) => res.send('Hello, Unit!'))
+	createServer(app).listen()
+}
 
-createServer(app).listen()
+const koa = () => {
+	const app = new (require('koa'))()
+
+	app.use(ctx => ctx.body = 'Hello, Unit! koa');
+
+	createServer(app.callback()).listen()
+}
+
+const fastify = () => {
+	const app = require("fastify")({
+		serverFactory: (handler) => createServer(handler)
+	});
+
+	app.get("/", (req, reply) => {
+	  reply.send('Hello, Unit! fastify');
+	});
+
+	app.ready();
+}
+
+express()
