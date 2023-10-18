87ade86e1bff7c73a26a3b4efec9dcdccfdbaec1
StevieYu <stevieyu@users.noreply.github.com>
2023-10-18 16:20:35 +0800

    Create china-ubuntu-php.sh

---
+++ china-ubuntu-php.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+apt install -y software-properties-common
+  
+add-apt-repository -y ppa:ondrej/php
+sed -i 's/ppa/hono.dgcf.link/ppa/' /etc/apt/sources.list.d/ondrej-ubuntu-php-focal.list
+  
+apt -y install php8.2-cli
+  
+curl -k -o /usr/local/bin/composer https://mirrors.tencent.com/composer/composer.phar
+chmod a+x /usr/local/bin/composer
+
+composer config -g repositories.packagist composer https://mirrors.tencent.com/composer/
