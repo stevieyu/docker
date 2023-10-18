b8d945c892c61d395a891519e474f47afcd60c8f
StevieYu <stevieyu@users.noreply.github.com>
2023-10-18 17:16:24 +0800

    Update china-ubuntu-php.sh

--- china-ubuntu-php.sh
+++ china-ubuntu-php.sh
@@ -8,7 +8,8 @@ add-apt-repository -y ppa:ondrej/php
 sed -i 's/ppa/hono.dgcf.link\/ppa/' /etc/apt/sources.list.d/*-focal.list
 apt update
   
-apt install -y php8.2-cli
+apt install -y php8.2-cli php8.2-dom php8.2-sqlite3 php8.2-mysql php8.2-curl
+sed -i 's/off/on/' /etc/php/8.2/cli/conf.d/10-opcache.ini
   
 curl -k -o /usr/local/bin/composer https://mirrors.tencent.com/composer/composer.phar
 chmod a+x /usr/local/bin/composer
