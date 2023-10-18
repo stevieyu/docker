226fde0d392a845bdd2363907398f002cd21d79c
StevieYu <stevieyu@users.noreply.github.com>
2023-10-18 18:43:17 +0800

    Update china-ubuntu-php.sh

--- china-ubuntu-php.sh
+++ china-ubuntu-php.sh
@@ -10,10 +10,18 @@ apt update
   
 apt install -y php8.2-cli php8.2-dom php8.2-sqlite3 php8.2-mysql php8.2-curl
 sed -i 's/opcache.jit/;opcache.jit/' /etc/php/8.2/cli/conf.d/10-opcache.ini
-  
-curl -k -o /usr/local/bin/composer https://mirrors.tencent.com/composer/composer.phar
+
+
+ping -c 1 -W 1 mirrors.tencent.com > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+ mirror=http://mirrors.tencentyun.com/composer/
+else
+ mirror=http://mirrors.tencent.com/composer/
+fi
+
+curl -k -o /usr/local/bin/composer ${mirror}composer.phar
 chmod a+x /usr/local/bin/composer
 
 export COMPOSER_ALLOW_SUPERUSER=1
 echo 'export COMPOSER_ALLOW_SUPERUSER='$COMPOSER_ALLOW_SUPERUSER >> ~/.bashrc >> ~/.zshrc
-composer config -g repositories.packagist composer https://mirrors.tencent.com/composer/
+composer config -g repositories.packagist composer ${mirror}
