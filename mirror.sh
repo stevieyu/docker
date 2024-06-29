3745157e91ed312a9b44decd41efc19f737c1aa1
StevieYu <stevieyu@users.noreply.github.com>
2024-06-29 11:18:24 +0800

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -16,7 +16,7 @@ with_sudo() {
 }
 
 if command -v apt &> /dev/null || command -v apk &> /dev/null; then
-  find /etc -regex '.*\(repositories\|sources.list\(.d\/.*\)?\)$' | with_sudo xargs sed -i -E 's/(archive|security).ubuntu.com|(deb).debian.org|dl-cdn.alpinelinux.org/'$mirror_OS'/g'
+  with_sudo find /etc -regex '.*\(repositories\|sources.list\(.d\/.*\)?\)$' | with_sudo xargs sed -i -E 's/(archive|security).ubuntu.com|(deb).debian.org|dl-cdn.alpinelinux.org/'$mirror_OS'/g'
   echo "设置apt或者apk镜像"
 fi
 
