bb41c5c9f34791fdc0a2929f969c54a9f0ad6ee0
StevieYu <stevieyu@users.noreply.github.com>
2024-06-29 09:23:43 +0800

    Update mirror.sh

--- mirror.sh
+++ mirror.sh
@@ -6,7 +6,7 @@ mirror_GO=goproxy.cn
 # 使用逻辑或检查 apt 和 apk 是否存在任意一个
 if command -v apt &> /dev/null || command -v apk &> /dev/null; then
   echo "设置apt或者apk镜像"
-  find /etc -regex '.*\(repositories\|sources.list\(.d\/.*\)?\)$' | xargs sed -i -E 's/(archive|security).ubuntu.com|(deb).debian.org|dl-cdn.alpinelinux.org/$mirror_OS/g'
+  find /etc -regex '.*\(repositories\|sources.list\(.d\/.*\)?\)$' | xargs sed -i -E 's/(archive|security).ubuntu.com|(deb).debian.org|dl-cdn.alpinelinux.org/'$mirror_OS'/g'
 fi
 
 if command -v go &> /dev/null; then
