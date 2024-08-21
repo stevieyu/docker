721e06bf654c31540b621f661f75cabf14faf136
StevieYu <stevieyu@users.noreply.github.com>
2024-08-21 08:30:33 +0800

    Update devbox.sh

--- devbox.sh
+++ devbox.sh
@@ -7,29 +7,18 @@ use_sudo() { [ "$(id -u)" = "0" ] || sudo "$@"; }
 
 if ! command -v xz &> /dev/null; then
   use_sudo apt install -y xz-utils
-
-
-  if ! command -v nix &> /dev/null; then
-    sh <(curl -L https://$MIRROR_HOST/nix/latest/install) --daemon --yes --no-channel-add && \
-    echo "substituters = https://$MIRROR_HOST/nix-channels/store https://cache.nixos.org/" >> /etc/nix/nix.conf && \
-    echo "https://$MIRROR_HOST/nix-channels/nixpkgs-unstable nixpkgs" > $HOME/.nix-channels && \
-    export PATH=$PATH:$HOME/.nix-profile/bin && \
-    nix-channel --update
-
-
-    if ! command -v devbox &> /dev/null; then
-      nix-env -iA nixpkgs.devbox && devbox global shellenv --recompute && \
-      echo 'eval "$(devbox global shellenv)"' >> $HOME/.zshrc >> $HOME/.bashrc.d/devbox
-    fi
-
-
-    
-  fi
-  
-
-
-
-  
 fi
 
 
+if ! command -v nix &> /dev/null; then
+  sh <(curl -L https://$MIRROR_HOST/nix/latest/install) --daemon --yes --no-channel-add && \
+  echo "substituters = https://$MIRROR_HOST/nix-channels/store https://cache.nixos.org/" >> /etc/nix/nix.conf && \
+  echo "https://$MIRROR_HOST/nix-channels/nixpkgs-unstable nixpkgs" > $HOME/.nix-channels && \
+  export PATH=$PATH:$HOME/.nix-profile/bin && \
+  nix-channel --update
+fi
+
+if ! command -v devbox &> /dev/null; then
+  nix-env -iA nixpkgs.devbox && devbox global shellenv --recompute && \
+  echo 'eval "$(devbox global shellenv)"' >> $HOME/.zshrc >> $HOME/.bashrc.d/devbox
+fi
