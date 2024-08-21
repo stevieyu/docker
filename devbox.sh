eb567168f0ceb3768909c87549289ffc4b442698
StevieYu <stevieyu@users.noreply.github.com>
2024-08-21 09:01:59 +0800

    Update devbox.sh

--- devbox.sh
+++ devbox.sh
@@ -5,12 +5,16 @@ MIRROR_HOST=mirrors.bfsu.edu.cn
 
 use_sudo() { [ "$(id -u)" = "0" ] || sudo "$@"; }
 
-if ! command -v xz &> /dev/null 2>&1; then
+if command -v xz &> /dev/null 2>&1; then
+  echo 'xz已安装'
+else
   use_sudo apt install -y xz-utils
 fi
 
 
-if command -v xz &> /dev/null 2>&1 && ! command -v nix &> /dev/null 2>&1; then
+if command -v xz &> /dev/null 2>&1 && command -v nix &> /dev/null 2>&1; then
+  echo 'nix已安装'
+else
   sh <(curl -L https://$MIRROR_HOST/nix/latest/install) --daemon --yes --no-channel-add && \
   echo "substituters = https://$MIRROR_HOST/nix-channels/store https://cache.nixos.org/" >> /etc/nix/nix.conf && \
   echo "https://$MIRROR_HOST/nix-channels/nixpkgs-unstable nixpkgs" > $HOME/.nix-channels && \
@@ -20,6 +24,8 @@ fi
 
 
 if command -v nix-env &> /dev/null 2>&1 && ! command -v devbox &> /dev/null 2>&1; then
+  echo 'devbox已安装'
+else
   nix-env -iA nixpkgs.devbox && devbox global shellenv --recompute && \
   echo 'eval "$(devbox global shellenv)"' >> $HOME/.zshrc >> $HOME/.bashrc.d/devbox
 fi
