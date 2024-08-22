b766bea359567f7ef66ad1c059836527682d9f24
StevieYu <stevieyu@users.noreply.github.com>
2024-08-22 09:01:34 +0800

    Update devbox.sh

--- devbox.sh
+++ devbox.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # curl -L https://fastly.jsdelivr.net/gh/stevieyu/docker/devbox.sh | bash -s --
 
-export MIRROR_HOST=mirrors.ustc.edu.cn
+
 
 use_sudo() { [ "$(id -u)" = "0" ] || sudo "$@"; }
 
 use_sudo apt install -y xz-utils
 
-
 export PATH=$PATH:$HOME/.nix-profile/bin && \
 sh <(curl -L https://mirrors.bfsu.edu.cn/nix/latest/install) --daemon --yes --no-channel-add && \
+MIRROR_HOST=mirrors.ustc.edu.cn && \
 echo "substituters = https://$MIRROR_HOST/nix-channels/store https://cache.nixos.org/" >> /etc/nix/nix.conf && \
 nix-channel --add https://$MIRROR_HOST/nix-channels/nixpkgs-unstable nixpkgs && \
 nix-channel --update
